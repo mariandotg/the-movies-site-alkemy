4f4f39211ab2c3e20cc0260d9906205244221729
mariandotg <marianguillaume.m@gmail.com>
2022-05-08 12:29:43 -0300

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
+import List from "./components/List";
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/list" component={List} />
+    </Switch>
   );
 };
 
