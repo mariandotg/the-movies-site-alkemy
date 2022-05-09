92062f9897639a0b323d11b7082982eb2b2f97c2
mariandotg <marianguillaume.m@gmail.com>
2022-05-09 14:20:22 -0300

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://instagram.com" rel="noopener noreferrer">
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p>Copyright mariandotg</p>
+    </footer>
+  );
+};
+
+export default Footer;
