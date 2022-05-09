8ebfdb1d96a4fc3997da7d6d81ade231cdd4fde4
Mariano Guillaume <84543990+mariandotg@users.noreply.github.com>
2022-05-09 16:16:26 -0300

    Header component (#3)
    * Added Header component
    
    * Implemented header into the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import List from "./components/List";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/list" component={List} />
