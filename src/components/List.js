8847121210ce3c9d6f047e706c0f7b98fc39e455
mariandotg <marianguillaume.m@gmail.com>
2022-05-09 11:35:54 -0300

    Added token verification

--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,16 @@
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
+
 const List = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.get("token");
+    if (token === null) {
+      history.push("/");
+    }
+  }, []);
+
   return <h2>Listado</h2>;
 };
 
