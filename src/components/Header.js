53461b1fb4cd17eda1f4dc301b42fb45868372e6
mariandotg <marianguillaume.m@gmail.com>
2022-05-09 14:06:25 -0300

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/list"}>List</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
